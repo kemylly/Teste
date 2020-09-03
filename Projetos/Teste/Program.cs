d29ef6d608ed8a012e2b2f611fca6f1ba45b9635
kemylly <kemylly@users.noreply.github.com>
2020-09-03 12:40:05 -0300

    Mudança de pasta

---
+++ Projetos/Teste/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hello
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Okay");
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
